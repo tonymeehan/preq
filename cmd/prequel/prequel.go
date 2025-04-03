5600007aee81d2088544a1a21fc30eb766797ee2
Tony Meehan <tonymeehan@users.noreply.github.com>
2025-04-02 23:30:01 -0500

    Fix json logs

--- cmd/prequel/prequel.go
+++ cmd/prequel/prequel.go
@@ -112,11 +112,16 @@ func main() {
 
 	kong.Parse(&cli)
 
-	// Initialize logger first before any other logging
-	logs.InitLogger(
+	logOpts := []logs.InitOpt{
 		logs.WithLevel(cli.Level),
-		logs.WithPretty(),
-	)
+	}
+
+	if !cli.JsonLogs {
+		logOpts = append(logOpts, logs.WithPretty())
+	}
+
+	// Initialize logger first before any other logging
+	logs.InitLogger(logOpts...)
 
 	switch {
 	case cli.Version:
